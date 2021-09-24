3be955648c416c274242db5f0de56f8c0563bfc2
nasimakhtar <nasim@backbase.com>
2021-09-25 00:32:23 +0800

    Initial commit

---
+++ src/test/java/com/example/electroappliance6/ElectroAppliance6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electroappliance6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectroAppliance6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
